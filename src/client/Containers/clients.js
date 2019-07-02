057b184be4467d3c273eca5a213a211e4ed4dcff
chwello <dimon71.chess@gmail.com>
2019-07-02 12:12:48 +0300

    add structure

---
+++ src/client/Containers/clients.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Clients extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Clientfffs</h2>
+      </div>
+    );
+  }
+}
