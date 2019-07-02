82f5b0b796bfa713ef62a57a96ed9bd9d63cd2ba
JarponOK <fenix667@yandex.ru>
2019-07-02 14:27:07 +0300

    add server

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
-const os = require('os');
+const { MongoClient } = require('mongodb');
+const bodyParser = require('body-parser');
+const mongodb = require('./config/db.js');
 
 const app = express();
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+let db;
+MongoClient.connect(mongodb.url, (err, client) => {
+  if (err) return console.log(err);
+  db = client.db('DXPractice');
+  app.listen(8080, () => {
+    console.log('listen port 8080');
+  });
+});
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+
+app.post('/api/patient', (req, res) => {
+  const details = {
+    name: 1, lastname: 1, phone: 1
+  };
+  
+  db.collection('patient').find({}, details).toArray((err, result) => {
+    if (err) return console.log(err);
+    res.send(result);
+  });
+});
