f2cad6f840f001a414623730bfa7a9925028875b
JarponOK <fenix667@yandex.ru>
2019-07-02 14:46:17 +0300

    add birthday

--- src/data.js
+++ src/data.js
@@ -2,12 +2,13 @@ export const dataClient = [{
   name: 'Deric',
   lastname: 'Lawson',
   phone: '765-052-5230',
+  birthday: '1983-04-19T20:00:00.000+00:00',
   dateApt: '2019-05-24T02:09:00.000+00:00'
 }, {
   name: 'Eric',
   lastname: 'Garis',
   phone: '765-352-5430',
-  email: 'parintn@email.com',
+  birthday: '1983-05-28T20:00:00.000+00:00',
   dateApt: '2019-04-12T02:09:00.000+00:00'
 }];
 
@@ -29,4 +30,23 @@ export const dataSheduler = [{
   operation: 'Cosmetic Composite resin restoration',
   location: 'Room 24',
   note: '',
+}];
+
+export const dateTreatment = [{
+  diagnosis: [{
+    operation: 'Blood test',
+    cost: '25'
+  }],
+  restoration: [
+    'Dental dam', 'Sealant'
+  ],
+  root_canal: [
+    'Direct pulp capping', 'Root canal re-treatment (molar'
+  ],
+  hygiene: [
+    'Gum grafting', 'Operculectomy'
+  ],
+  whitening: [
+    'Internal bleaching', 'At-home whitening kit (Zoom)'
+  ]
 }];
