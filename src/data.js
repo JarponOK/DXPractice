28a77d0921f93b1be679521d2d0f2b2b0c55217d
JarponOK <fenix667@yandex.ru>
2019-07-02 14:20:11 +0300

    add stub

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const dataClient = [{
+  name: 'Deric',
+  lastname: 'Lawson',
+  phone: '765-052-5230',
+  dateApt: '2019-05-24T02:09:00.000+00:00'
+}, {
+  name: 'Eric',
+  lastname: 'Garis',
+  phone: '765-352-5430',
+  email: 'parintn@email.com',
+  dateApt: '2019-04-12T02:09:00.000+00:00'
+}];
+
+export const dataSheduler = [{
+  name: 'Deric',
+  lastname: 'Lawson',
+  phone: '765-052-5230',
+  start_time: '2019-05-03T02:09:00.000+00:00',
+  end_time: '2019-05-03T21:11:00.000+00:00',
+  operation: 'Cosmetic Composite resin restoration',
+  location: 'Room 24',
+  note: '',
+}, {
+  name: 'Eric',
+  lastname: 'Garis',
+  phone: '765-352-5430',
+  start_time: '2019-05-04T02:12:00.000+00:00',
+  end_time: '2019-05-04T21:13:00.000+00:00',
+  operation: 'Cosmetic Composite resin restoration',
+  location: 'Room 24',
+  note: '',
+}];
