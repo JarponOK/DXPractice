a2e7804d4368f2e8b8a7311f3fb9a668dd3defb9
chwello <dimon71.chess@gmail.com>
2019-07-11 15:26:41 +0300

    add menu and themeprovider

--- src/client/index.js
+++ src/client/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React,{PureComponent} from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import {store} from "./Store/configureStore"
 import App from './Components/App';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import theme from './themes/theme';
 
-
+export default class Header extends PureComponent {
+    render() {
+      return (
+        <Provider store={store}>
+        <MuiThemeProvider theme={theme}>
+          <App/>
+        </MuiThemeProvider>
+        </Provider>
+      );
+    }
+  }
 
 ReactDOM.render(
-<Provider store={store}>
-    <App /> 
-    </Provider>, 
+    <Header /> , 
     document.getElementById('root'));
 
---
+++ src/client/themes/theme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from 'material-ui/styles/';
+import indigo from 'material-ui/colors/indigo';
+import purple from 'material-ui/colors/purple';
+import red from 'material-ui/colors/red';
+
+export default createMuiTheme({
+    palette: {
+        primary: { main: purple[500] }, 
+        secondary: { main: '#11cb5f' }, 
+      },
+        } 
+    )
