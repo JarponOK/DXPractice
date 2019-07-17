1ad769124e204a62e633913b2d2c0e419be272e3
FrodoAFK <alexsmirnov.1999@mail.ru>
2019-07-17 12:19:16 +0300

    Add grid

--- src/client/Containers/clients.jsx
+++ src/client/Containers/clients.jsx
@@ -2,9 +2,27 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import { Paper, Grid, Typography, Button, TextField, InputAdornment, IconButton } from '@material-ui/core';
+import {
+  Paper, Grid, Typography, Button, TextField, InputAdornment, IconButton
+} from '@material-ui/core';
+import {
+  PagingState,
+  IntegratedPaging,
+  SortingState,
+  SearchState,
+  IntegratedFiltering,
+  IntegratedSorting,
+} from '@devexpress/dx-react-grid';
+import {
+  Grid as GridData,
+  Table,
+  Toolbar,
+  SearchPanel,
+  TableHeaderRow,
+} from '@devexpress/dx-react-grid-material-ui';
 import Search from '@material-ui/icons/Search';
 import { clientsFetchData } from '../Actions/itemClients';
+import { URL_CLIENTS } from './const';
 
 
 const useStyles = makeStyles(theme => ({
@@ -26,6 +44,8 @@ const useStyles = makeStyles(theme => ({
 
   }
 }));
+
+const getCellValue = data => `${data.name} ${data.lastname}`;
 
 function ClientsBody() {
   const classes = useStyles();
@@ -54,13 +74,8 @@ function ClientsBody() {
               }}
             />
           </Grid>
-          <Button className={classes.button} color='secondary' variant="contained">Add new patient</Button>
+          <Button className={classes.button} color="secondary" variant="contained">Add new patient</Button>
         </Grid>
-      </Grid>
-
-      {/* Body */}
-      <Grid item className={classes.root}>
-        <Typography>Body</Typography>
       </Grid>
     </Grid>
   );
@@ -68,12 +83,20 @@ function ClientsBody() {
 class Clients extends Component {
   componentDidMount() {
     const { fetchData } = this.props;
-    fetchData('http://localhost:8080/api/clients');
-
+    fetchData(URL_CLIENTS);
   }
 
   render() {
-    console.log(this.props);
+    const { items } = this.props;
+    const { rows, columns } = {
+      columns: [
+        { name: 'name', getCellValue, title: 'Patient name' },
+        { name: 'birthday', title: 'Birthday' },
+        { name: 'phone', title: 'Phone' },
+        { name: 'lastAppt', title: 'Last appt' },
+      ],
+      rows: items,
+    };
     const { hasErrored, isLoading } = this.props;
     if (hasErrored) {
       return <p>Sorry! There was an error loading the items</p>;
@@ -83,11 +106,34 @@ class Clients extends Component {
       return <p>Loading…</p>;
     }
 
-    return (<ClientsBody />);
+    return (
+      <Paper>
+        <ClientsBody />
+        <GridData
+          rows={rows}
+          columns={columns}
+        >
+          <PagingState
+            defaultCurrentPage={0}
+            pageSize={6}
+          />
+          <SortingState
+            defaultSorting={[{ columnName: 'name', direction: 'asc' }]}
+          />
+          <IntegratedSorting />
+          <IntegratedPaging />
+          <IntegratedFiltering />
+          <Table />
+          <Toolbar />
+          <TableHeaderRow showSortingControls />
+        </GridData>
+      </Paper>
+    );
   }
 }
 
 Clients.propTypes = {
+  items: PropTypes.arrayOf.isRequired,
   fetchData: PropTypes.func.isRequired,
   hasErrored: PropTypes.bool.isRequired,
   isLoading: PropTypes.bool.isRequired
